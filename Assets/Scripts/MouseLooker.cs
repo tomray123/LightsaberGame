2435ddc88381e7fb55bdc5ffe0aa728210462125
Artem Pivko <paw-line@yandex.ru>
2020-12-18 00:01:19 +0300

    Created mouse rotation controls

---
+++ Assets/Scripts/MouseLooker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLooker : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //target.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
