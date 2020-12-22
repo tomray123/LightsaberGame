c901f6b107f8bb80db3c199b476ea6a34b56d204
Григорий Стрельцов <gastreltsov1998@gmail.com>
2020-12-22 14:08:43 +0300

    Player and Player's Settings were added

--- Assets/Scripts/JoystickController.cs
+++ Assets/Scripts/JoystickController.cs
@@ -17,21 +17,27 @@ public class JoystickController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButton(0)) //Change this to (Input.touchCount > 0) in order to switch PC to mobile
+        if (Input.GetMouseButton(0)) //Change this to (Input.touchCount > 0) in order to switch PC to mobile
         {
             Vector3 touchPos = Input.mousePosition; //Also change this to Input.GetTouch(0).position
             targetVector = touchPos - transform.position;
+            plController.targetMove = targetVector;
 
-            if(targetVector.magnitude < 100)
+            if (targetVector.magnitude < 100)
             {
                 touchMarker.transform.position = touchPos;
-                plController.targetMove = targetVector;
             }
+            else
+            {
+                float newX = transform.position.x + Mathf.Cos(Mathf.Atan2(targetVector.x, targetVector.y)) * 100;
+                float newY = transform.position.y + Mathf.Sin(Mathf.Atan2(targetVector.x, targetVector.y)) * 100;
+                touchMarker.transform.position = new Vector3(newX, newY, transform.position.z);
+            }
+        }
         else
         {
             touchMarker.transform.position = transform.position;
             //plController.targetMove = new Vector3(0, 0, 0);
-        }
         }
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 targetMove;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.up = targetMove;
+    }
+}
