7ad00f0d9c1cfa8f63bc9ac515f529c8a4e20621
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-01-08 15:54:51 +0300

    Added bullet script and reflection

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float speed = 0.1f;
+    Vector3 direction;
+
+    private void Start()
+    {
+        direction = transform.up;  //Change it later
+    }
+    void FixedUpdate()
+    {
+        transform.Translate(direction * speed, Space.World);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Got it");
+        ContactPoint2D contact = collision.GetContact(0);
+        direction = Vector3.Reflect(direction, contact.normal);
+        transform.up = direction;
+    }
+}
