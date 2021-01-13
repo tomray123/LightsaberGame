5c831196b6028f5bcaf8484122b6d612a51da88e
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-01-13 21:28:25 +0300

    New bullet sprite was added and the correct bullet reflection was implemented

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    float speed = 0.12f;
+    public float speed = 0.15f;
     Vector3 direction;
+    public GameObject frontPoint;
+
+    //public float bit = 0.01f;
+    //private bool timeToStop = false;
 
     private void Start()
     {
         direction = transform.up;  //Change it later
+        Vector2[] colPoints = transform.GetComponent<EdgeCollider2D>().points;
+        frontPoint.transform.localPosition = colPoints[0];
     }
     void FixedUpdate()
     {
@@ -22,7 +28,34 @@ public class Bullet : MonoBehaviour
         {
             Debug.Log("Got it");
             direction = Vector3.Reflect(direction, other.transform.up);
+            transform.position = frontPoint.transform.position;
             transform.up = direction;
+            
+            //timeToStop = true;
         }
     }
+
+
+    /*IEnumerator SmoothMove()
+    {
+        int i;
+        for (i = 0; i < speed / bit; i++)
+        {
+            yield return new WaitForSeconds(Time.deltaTime / (speed / bit));
+            transform.Translate(direction * bit * Time.deltaTime, Space.World);
+            if (timeToStop)
+            {
+                break;
+            }
+        }
+        if (timeToStop)
+        {
+            for (; i < speed / bit; i++)
+            {
+                transform.Translate(direction * bit * Time.deltaTime, Space.World);
+            }
+        }
+        timeToStop = false;
+        
+    }*/
 }
