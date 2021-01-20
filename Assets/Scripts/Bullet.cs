059c5fb25eb02432a6bc283f4030372f6e36ed2d
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-01-20 20:33:38 +0300

    Test stuff was deleted, bullet prefab was created and player RB was set to kinematic

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,58 +6,39 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 0.15f;
     Vector3 direction;
+
     public GameObject frontPoint;
     public GameObject target;
 
-    //public float bit = 0.01f;
-    //private bool timeToStop = false;
+    private Rigidbody2D rb;
 
     private void Start()
     {
         direction = target.transform.position - transform.position;
         transform.up = direction;  //Change it later
+
         Vector2[] colPoints = transform.GetComponent<EdgeCollider2D>().points;
         frontPoint.transform.localPosition = colPoints[0];
+
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = direction.normalized * speed;
     }
+
     void FixedUpdate()
     {
-        transform.Translate(direction.normalized * speed, Space.World);
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("LightSaber")) 
-        {
-            Debug.Log("Got it");
-            direction = Vector3.Reflect(direction, other.transform.up);
-            transform.position = frontPoint.transform.position;
-            transform.up = direction;
-            
-            //timeToStop = true;
-        }
-    }
-
-
-    /*IEnumerator SmoothMove()
-    {
-        int i;
-        for (i = 0; i < speed / bit; i++)
-        {
-            yield return new WaitForSeconds(Time.deltaTime / (speed / bit));
-            transform.Translate(direction * bit * Time.deltaTime, Space.World);
-            if (timeToStop)
-            {
-                break;
-            }
-        }
-        if (timeToStop)
-        {
-            for (; i < speed / bit; i++)
-            {
-                transform.Translate(direction * bit * Time.deltaTime, Space.World);
-            }
-        }
-        timeToStop = false;
         
-    }*/
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("LightSaber")) 
+        {
+            direction = Vector3.Reflect(direction, other.transform.up);
+            transform.position = other.GetContact(0).point;
+            transform.up = direction;
+            rb.velocity = direction.normalized * speed;
+        }
+    }
+
 }
