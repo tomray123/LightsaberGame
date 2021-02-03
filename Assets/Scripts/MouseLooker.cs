34a650f1dd88326a0a9543798f6c31ac86e858ad
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-02-03 20:10:45 +0300

    Mouse Look controller was added

--- Assets/Scripts/MouseLooker.cs
+++ Assets/Scripts/MouseLooker.cs
@@ -6,6 +6,8 @@ public class MouseLooker : MonoBehaviour
 {
     public Transform target;
 
+    public PlayerController plController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +17,13 @@ public class MouseLooker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //target.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if (Input.GetMouseButton(0)) //Change this to (Input.touchCount > 0) in order to switch PC to mobile
+        {
+            //target.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            plController.targetMove = dir;
+            //var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 }
