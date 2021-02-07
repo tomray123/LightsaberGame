58430bdf94c80667ecdbb455680bdcefbd111e44
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-02-07 15:13:18 +0300

    Finished the UI

---
+++ Assets/Scripts/UI/PauseController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+
+    public GameObject PauseMenu;
+
+    public GameObject GameUI;
+
+    public GameObject joystick;
+
+    public void PauseGame()
+    {
+        IsGamePaused = true;
+        joystick.SetActive(false);
+        Time.timeScale = 0f;
+        PauseMenu.SetActive(true);
+        GameUI.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        GameUI.SetActive(true);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+        joystick.SetActive(true);
+    }
+
+    public void BackToMenu(string sceneName)
+    {
+        PauseMenu.SetActive(false);
+        GameUI.SetActive(true);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+        joystick.SetActive(true);
+        SceneManager.LoadScene(sceneName);
+    }
+}
