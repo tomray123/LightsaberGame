f6dc6269f0b6e8a52e68d6ffa41a46621642063d
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-03-14 15:51:23 +0300

    DOTween module was added and throw animations were implemented as test in the first level

--- Assets/Scripts/Control scripts/PlayerController.cs
+++ Assets/Scripts/Control scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,30 +9,51 @@ public class PlayerController : MonoBehaviour
 
     public float rotationSpeed = 5f;
 
+    public float ThrowTime = 5f;
+
     public bool isSmooth = false;
+
+    public Transform saber;
+
+    public bool isThrown;
+
+    public Vector3 _targetLocation = Vector3.zero;
+
+    Vector3 startPos;
+
+    DoTweenController tweenController;
+
+    private void Start()
+    {
+        saber = transform.GetChild(0);
+        isThrown = false;
+        startPos = saber.position;
+        tweenController = saber.GetComponent<DoTweenController>();
+    }
 
     void Update()
     {
-        float angle = Angle360(Vector3.up, targetMove, Vector3.right);
-
-        if (isSmooth)
+        if (tweenController.isThrowTweenCompleted)
         {
-            //third way to rotate
-            Quaternion targetQuater = Quaternion.Euler(0, 0, angle);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetQuater, rotationSpeed * Time.deltaTime);
-            
-        }
-        else
-        {
-            //first way to rotate
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
+            float angle = Angle360(Vector3.up, targetMove, Vector3.right);
 
-        //second way to rotate
-        //Quaternion targetQuater = Quaternion.Euler(0, 0, angle);
-        //transform.rotation  = Quaternion.Slerp(transform.rotation, targetQuater, Time.deltaTime * rotationSpeed);
+            if (isSmooth)
+            {
+                //third way to rotate
+                Quaternion targetQuater = Quaternion.Euler(0, 0, angle);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetQuater, rotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+                //first way to rotate
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
 
-        
+            if (Input.GetMouseButtonDown(1)) //Change this to (Input.touchCount > 1) in order to switch PC to mobile
+            {
+                StartCoroutine(tweenController.DoThrowAndRotate(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+            }
+        }
     }
 
     float Angle360(Vector3 from, Vector3 to, Vector3 right)
@@ -40,4 +62,51 @@ public class PlayerController : MonoBehaviour
         return (Vector3.Angle(right, to) < 90f) ? 360f - angle : angle;
     }
 
+    void ThrowSaber()
+    {
+
+        /*
+        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(targetPositionLocal);
+        targetPosition.z = 0f;
+        
+        Vector3 direction = targetPosition - saber.position;
+        Debug.DrawRay(saber.position, direction, Color.yellow);
+        float startSpeed = speed;
+        Vector3 startPosition = saber.position;
+
+        while (isThrown)
+        {
+            
+            speed = Mathf.Lerp(startSpeed, -1 * startSpeed, progress);
+            progress += step;
+            Debug.Log(speed);
+            saber.position = Vector3.MoveTowards(saber.position, targetPosition*2, Time.deltaTime * speed);
+            if (speed <= -1 * startSpeed)
+            {
+                speed = 0;
+                progress = 0;
+                isThrown = false;
+            }
+            yield return null;
+            
+            direction = targetPosition - saber.position;
+            if (direction.magnitude < 2 && progress < 1)
+            {
+                speed = Mathf.Lerp(startSpeed, -1*startSpeed, progress);
+                progress += step;
+                Debug.Log(speed);
+            }
+            saber.position = Vector3.MoveTowards(saber.position, targetPosition, Time.deltaTime * speed);
+            if (saber.position == targetPosition)
+            {
+                //speed = 0;
+               // progress = 0;
+                isThrown = false;
+            }
+            yield return null;
+            
+        }
+        */
+    }
+        
 }
---
+++ Assets/Scripts/DoTweenController.cs
@@ -0,0 +1,153 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public class DoTweenController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _targetLocation = Vector3.zero;
+
+    [Range(0.5f, 10.0f), SerializeField]
+    private float _moveDuration = 1.0f;
+
+    [SerializeField]
+    private float _rotate360Duration = 1.0f;
+
+    [SerializeField]
+    private Ease _moveEase = Ease.Linear;
+
+    [SerializeField]
+    private Ease _moveForwardEase = Ease.Linear;
+
+    [SerializeField]
+    private Ease _moveBackwardEase = Ease.Linear;
+
+    [SerializeField]
+    private Color _targetColor;
+
+    [Range(1.0f, 500.0f), SerializeField]
+    private float _scaleMultiplier = 3.0f;
+
+    [Range(1.0f, 10.0f), SerializeField]
+    private float _colorChangeDuration = 1.0f;
+
+    [SerializeField]
+    private DoTweenType _doTweenType = DoTweenType.MovementOneWay;
+
+    public bool isThrowTweenCompleted = true;
+
+    private enum DoTweenType
+    {
+        MovementOneWay,
+        MovementTwoWay,
+        MovementTwoWayWithSequence,
+        MovementOneWayColorChange,
+        MovementOneWayColorChangeAndScale
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isThrowTweenCompleted = true;
+        // Uncomment this to test and debug tween
+
+        /*
+        if (_doTweenType == DoTweenType.MovementOneWay)
+        {
+            if (_targetLocation == Vector3.zero)
+                _targetLocation = transform.position;
+            transform.DOMove(_targetLocation, _moveDuration).SetEase(_moveEase);
+        }
+        else if (_doTweenType == DoTweenType.MovementTwoWay)
+        {
+            if (_targetLocation == Vector3.zero)
+                _targetLocation = transform.position;
+            StartCoroutine(MoveWithBothWays());
+        }
+        else if (_doTweenType == DoTweenType.MovementTwoWayWithSequence)
+        {
+            if (_targetLocation == Vector3.zero)
+                _targetLocation = transform.position;
+            Vector3 originalLocation = transform.position;
+            DOTween.Sequence()
+                .Append(transform.DOMove(_targetLocation, _moveDuration).SetEase(_moveEase))
+                .Append(transform.DOMove(originalLocation, _moveDuration).SetEase(_moveEase));
+        }
+        else if (_doTweenType == DoTweenType.MovementOneWayColorChange)
+        {
+            if (_targetLocation == Vector3.zero)
+                _targetLocation = transform.position;
+            DOTween.Sequence()
+                .Append(transform.DOMove(_targetLocation, _moveDuration).SetEase(_moveEase))
+                .Append(transform.GetComponent<Renderer>().material
+                .DOColor(_targetColor, _colorChangeDuration).SetEase(_moveEase));
+        }
+        else if (_doTweenType == DoTweenType.MovementOneWayColorChangeAndScale)
+        {
+            if (_targetLocation == Vector3.zero)
+                _targetLocation = transform.position;
+            DOTween.Sequence()
+                .Append(transform.DOMove(_targetLocation, _moveDuration).SetEase(_moveEase))
+                .Append(transform.DOScale(_scaleMultiplier, _moveDuration / 2.0f).SetEase(_moveEase))
+                .Append(transform.GetComponent<Renderer>().material
+                .DOColor(_targetColor, _colorChangeDuration).SetEase(_moveEase));
+        }
+        */
+    }
+
+    public void Rotate360()
+    {
+        transform.DORotate(new Vector3(0, 0, 360), 0.1f, RotateMode.Fast).SetEase(Ease.Linear).SetLoops(-1, LoopType.Incremental);
+    }
+
+    public void MoveTwoWays(Vector3 targetPosition)
+    {
+        if (targetPosition == Vector3.zero)
+            targetPosition = transform.position;
+        StartCoroutine(MoveWithBothWays(targetPosition));
+    }
+
+    private IEnumerator MoveWithBothWays()
+    {
+        Vector3 originalLocation = transform.position;
+        transform.DOMove(_targetLocation, _moveDuration).SetEase(_moveEase);
+        yield return new WaitForSeconds(_moveDuration);
+        transform.DOMove(originalLocation, _moveDuration).SetEase(_moveEase);
+    }
+
+    private IEnumerator MoveWithBothWays(Vector3 targetPosition)
+    {
+        targetPosition.z = 0f;
+        Vector3 originalLocation = transform.position;
+        transform.DOMove(targetPosition, _moveDuration).SetEase(_moveEase);
+        yield return new WaitForSeconds(_moveDuration);
+        transform.DOMove(originalLocation, _moveDuration).SetEase(_moveEase);
+    }
+
+    /*
+    public bool ThrowWithRotation(Vector3 targetPosition)
+    {
+        StartCoroutine(DoThrowAndRotate(targetPosition));
+        return isThrowTweenCompleted;
+    }
+    */
+
+    public IEnumerator DoThrowAndRotate(Vector3 targetPosition)
+    {
+        Quaternion originalRotation = transform.localRotation;
+        isThrowTweenCompleted = false;
+        Tween Rotation = transform.DORotate(new Vector3(0, 0, 360), _rotate360Duration, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1);
+        if (targetPosition == Vector3.zero)
+            targetPosition = transform.position;
+        targetPosition.z = 0f;
+        Vector3 originalLocation = transform.position;       
+        Tween forwardTween = transform.DOMove(targetPosition, _moveDuration).SetEase(_moveForwardEase);
+        //yield return forwardTween.WaitForCompletion();
+        yield return new WaitForSeconds(_moveDuration);
+        Tween backTween = transform.DOMove(originalLocation, _moveDuration).SetEase(_moveBackwardEase);
+        yield return backTween.WaitForCompletion();
+        Rotation.Kill();
+        isThrowTweenCompleted = true;
+        transform.localRotation = originalRotation;
+    }
+}
