4e63d628abacd771bf0ceda361b82b9d58dfb881
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-05-11 13:52:13 +0300

    Refactored MouseInputController script.

--- Assets/Scripts/Control scripts/MouseInputController.cs
+++ Assets/Scripts/Control scripts/MouseInputController.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 
 public class MouseInputController : MonoBehaviour
 {
+    // Singleton instance.
     public static MouseInputController instance;
 
     public Action OnSingleClick;
 
     public void Awake()
     {
+        // Creating singleton instance.
         if (instance == null)
         {
             instance = this;
         }
     }
 
+    // Calls the OnSingleClick action when single click.
     public void SingleClick()
     {
         if (OnSingleClick != null)
