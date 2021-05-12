9ce9a0b0e18d06d194c915185f17191c9fee5b62
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-05-12 19:28:14 +0300

    Refactored SpawnerEditor script.

--- Assets/Editor/SpawnerEditor.cs
+++ Assets/Editor/SpawnerEditor.cs
@@ -10,54 +10,48 @@ public class SpawnerEditor : Editor
     {
         //base.OnInspectorGUI();
 
+        // Setting a target script for custom editor.
         Spawner spawner = (Spawner)target;
 
         EditorGUILayout.LabelField("Set spawn period of the first object to all", EditorStyles.boldLabel);
         spawner.allSpawnPeriod = EditorGUILayout.Toggle("All Spawn Period", spawner.allSpawnPeriod);
-
-        /*EditorGUILayout.LabelField("Set time before the first shot of the first object to all", EditorStyles.boldLabel);
-        spawner.allTimeToInitialShoot = EditorGUILayout.Toggle("All Time To Initial Shoot", spawner.allTimeToInitialShoot);
-
-        EditorGUILayout.LabelField("Set time interval for shooting of the first object to all", EditorStyles.boldLabel);
-        spawner.allShootPeriod = EditorGUILayout.Toggle("All Shoot Period", spawner.allShootPeriod);*/
 
         EditorGUILayout.Space(10);
 
         int listSize = spawner.MyList.Count;
         listSize = EditorGUILayout.IntField("List Size", listSize);
 
+        // Check for local list size is equal to real spawn objects list size. 
         if (listSize != spawner.MyList.Count)
         {
+            // Adding objects in list.
             while (listSize > spawner.MyList.Count)
             {
                 SpawnObject sample = new SpawnObject();
                 spawner.MyList.Insert(spawner.MyList.Count, sample);
             }
+            // Or deleting objects from list.
             while (listSize < spawner.MyList.Count)
             {
                 spawner.MyList.RemoveAt(spawner.MyList.Count - 1);
             }
         }
-        /*
-        GameObject obj1 = spawner.MyList[0].obj;
-        GameObject obj2 = spawner.MyList[1].obj; ;
-        obj1 = (GameObject)EditorGUILayout.ObjectField(obj1, typeof(GameObject), false);
-        spawner.MyList[0].obj = obj1;
-        obj2 = (GameObject)EditorGUILayout.ObjectField(obj2, typeof(GameObject), false);
-        spawner.MyList[1].obj = obj2;
-        */
         
         for (int i=0; i < spawner.MyList.Count; i++)
         {
+            // Creating fields for gameobject, spawn position and spawn period.
             GameObject buf = (GameObject)EditorGUILayout.ObjectField(spawner.MyList[i].obj, typeof(GameObject), true);
             spawner.MyList[i].spawnPos = (Transform)EditorGUILayout.ObjectField(spawner.MyList[i].spawnPos, typeof(Transform), true);
             spawner.MyList[i].spawnPeriod = EditorGUILayout.FloatField("Spawn Period", spawner.MyList[i].spawnPeriod);
+
+            // Checking for adding a gameobject in field.
             if (buf != null)
             {
                 Enemy enemy = buf.GetComponent<Enemy>();
                 if (enemy != null && enemy.parameters.Count > 7)
                 {
-
+                    // If current object is different from the previous known object in list,
+                    // then set new default parameters to it.
                     if (buf != spawner.MyList[i].obj)
                     {
                         spawner.MyList[i].p1Name = enemy.parameters[0].name;
@@ -78,8 +72,10 @@ public class SpawnerEditor : Editor
                         spawner.MyList[i].p8Value = enemy.parameters[7].value;
                     }
 
+                    // Setting gameobject from field to list.
                     spawner.MyList[i].obj = buf;
 
+                    // Setting fields for every object's parameter.
                     GUILayout.BeginHorizontal();
                     EditorGUILayout.LabelField(spawner.MyList[i].p1Name);
                     spawner.MyList[i].p1Value = EditorGUILayout.FloatField(spawner.MyList[i].p1Value);
@@ -120,12 +116,8 @@ public class SpawnerEditor : Editor
                     spawner.MyList[i].p8Value = EditorGUILayout.FloatField(spawner.MyList[i].p8Value);
                     GUILayout.EndHorizontal();
                 }
-                
             }
-
             EditorGUILayout.Space(10);
-
         }
-
     }
 }
