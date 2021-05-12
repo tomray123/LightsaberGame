5b1e68986b640976b4b168b77b73460e286c1adc
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-05-12 18:09:27 +0300

    Refactored BasePlayerSettings script.

--- Assets/Scripts/BasePlayerSettings.cs
+++ Assets/Scripts/BasePlayerSettings.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class BasePlayerSettings : MonoBehaviour
 {
+    // For changing player's color.
     private Renderer rend;
 
+    // UI health bar slider.
     public HealthBar healthBar;
 
     public int maxHealth = 500;
 
     public int currentHealth;
 
+    // For checking whether palyer's killed or not.
     public static bool isKilled = false;
 
     private void Start()
@@ -23,24 +26,33 @@ public class BasePlayerSettings : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // Checking for bullet layer.
         if (other.gameObject.layer == 8)
         {
+            // Getting damage.
             currentHealth -= other.GetComponent<Bullet>().damage;
             healthBar.SetHealth(currentHealth);
             StartCoroutine(ChangeColor());
+            // Destroying the bullet.
             Destroy(other.gameObject);
         }
+
+        // Checking for explosion layer.
         if (other.gameObject.layer == 11)
         {
+            // Getting damage.
             currentHealth -= other.GetComponent<SimpleExplosion>().damage;
             healthBar.SetHealth(currentHealth);
             StartCoroutine(ChangeColor());
         }
-        if( other.gameObject.layer == 12)
+
+        // Checking for rocket layer.
+        if (other.gameObject.layer == 12)
         {
             GameObject explosion = other.gameObject.GetComponent<Rocket>().explosion;
             if (explosion != null)
             {
+                // Creating an explosion and destroying a rocket.
                 Instantiate(explosion, transform.position, Quaternion.identity);
                 Destroy(other);
             }
@@ -51,22 +63,28 @@ public class BasePlayerSettings : MonoBehaviour
         }
     }
 
+    // Changes the color of the object from normal to red and back 
+    // and also checks whether an object is dead and destroys it.
     IEnumerator ChangeColor()
     {
+        // Changing the color of the object from normal to red.
         for (float i = 1f; i >= 0; i -= 0.05f)
         {
             Color cl = new Color(1, i, i);
             rend.material.color = cl;
             yield return null;
         }
+        // Changing the color of the object from red to normal.
         for (float i = 0; i < 1f; i += 0.05f)
         {
             Color cl = new Color(1, i, i);
             rend.material.color = cl;
             yield return null;
         }
+        // Checking for death.
         if (currentHealth <= 0)
         {
+            // Destroying the object.
             isKilled = true;
             Destroy(gameObject);
         }
