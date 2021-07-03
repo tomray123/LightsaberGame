7309ec0e657c0302984634d2f7bf6fc81ca22b82
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-07-03 15:55:31 +0300

    Added Game Initializer in the main menu scene.

---
+++ Assets/Scripts/Control scripts/GameInit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameInit : MonoBehaviour
+{
+    [SerializeField]
+    private bool resetToInitialSettings = false;
+
+    private void Awake()
+    {
+        if (resetToInitialSettings)
+        {
+            PlayerPrefs.DeleteKey("init");
+        }
+
+        if (!PlayerPrefs.HasKey("init"))
+        {
+            // Setting records of each level in build to 0.
+            for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+            {
+                PlayerPrefs.SetInt("rec_lvl" + i.ToString(), 0);
+            }
+            PlayerPrefs.SetInt("init", 1);
+        }
+    }
+}
