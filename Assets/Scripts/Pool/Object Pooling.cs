0ccddb1375ae2f4e497d14779d483631d3ec13a6
Artem Pivko <paw-line@yandex.ru>
2021-09-18 15:39:31 +0300

    Added an object pool. Dont know if it works

---
+++ Assets/Scripts/Pool/Object Pooling.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[AddComponentMenu("Pool/ObjectPooling")]
+public class ObjectPooling
+{
+
+    #region Data
+    List<PoolObject> objects;
+    Transform objectsParent;
+    #endregion
+
+    #region Interface
+    public void Initialize(int count, PoolObject sample, Transform objects_parent)
+    {
+        objects = new List<PoolObject>();
+        objectsParent = objects_parent;
+        for (int i = 0; i < count; i++)
+        {
+            AddObject(sample, objects_parent);
+        }
+    }
+
+
+    public PoolObject GetObject()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            if (objects[i].gameObject.activeInHierarchy == false)
+            {
+                return objects[i];
+            }
+        }
+        AddObject(objects[0], objectsParent);
+        return objects[objects.Count - 1];
+    }
+    #endregion
+
+    #region Methods
+    void AddObject(PoolObject sample, Transform objects_parent)
+    {
+        GameObject temp;
+        temp = GameObject.Instantiate(sample.gameObject);
+        temp.name = sample.name;
+        temp.transform.SetParent(objects_parent);
+        objects.Add(temp.GetComponent<PoolObject>());
+        if (temp.GetComponent<Animator>())
+            temp.GetComponent<Animator>().StartPlayback();
+        temp.SetActive(false);
+    }
+    #endregion
+
+}
