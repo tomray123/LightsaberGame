9f02764f76c680557de6b4ee3967c0f2a60944fc
Gregory <gastreltsov1998@gmail.com>
2022-01-09 16:10:03 +0300

    Added swiping to levels menu in Main Menu.

--- Assets/Scripts/UI/Level Menu/PageSwiper.cs
+++ Assets/Scripts/UI/Level Menu/PageSwiper.cs
@@ -10,56 +10,61 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
     [SerializeField]
     private float easing = 0.5f;
     [SerializeField]
-    private int totalPages = 1;
-    [SerializeField]
     private Canvas canvas;
 
-    private Vector3 panelLocation;
+    private Vector3 lastPanelLocation;
+    private RectTransform levelHolderTransform;
+    private int totalPages = 1;
     private int currentPage = 1;
-    private Camera cam;
     private int localWidth;
 
     // Start is called before the first frame update
     void Start()
     {
-        panelLocation = transform.position;
-        cam = Camera.main;
+        canvas = FindObjectOfType<Canvas>();
+        levelHolderTransform = GetComponent<RectTransform>();
+        totalPages = transform.childCount;
+        lastPanelLocation = levelHolderTransform.anchoredPosition;
         localWidth = (int)(Screen.width / canvas.scaleFactor);
     }
 
     public void OnDrag(PointerEventData data)
     {
-        Vector3 pressPosition = cam.ScreenToWorldPoint(data.pressPosition);
-        Vector3 position = cam.ScreenToWorldPoint(data.position);
+        // Changing panel postion when dragging.
+        Vector3 pressPosition = data.pressPosition;
+        Vector3 position = data.position;
         float difference = pressPosition.x - position.x;
-        transform.position = panelLocation - new Vector3(difference, 0, 0);
+        levelHolderTransform.anchoredPosition = lastPanelLocation - new Vector3(difference, 0, 0);
     }
 
     public void OnEndDrag(PointerEventData data)
     {
-        panelLocation = transform.position;
+        // Calculating deviation percent.
         float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
         if (Mathf.Abs(percentage) >= percentThreshold)
         {
-            Vector3 newLocation = panelLocation;
+            Vector3 newLocation = lastPanelLocation;
+
+            // Swiping to the next page.
             if (percentage > 0 && currentPage < totalPages)
             {
                 currentPage++;
-                newLocation += cam.ScreenToWorldPoint(new Vector3(-localWidth, 0, 0));
-                print("Old location " + transform.position + " New location " + newLocation);  // REMOVE
+                newLocation += new Vector3(-localWidth, 0, 0);
             }
+            // Swiping to the previous page.
             else if (percentage < 0 && currentPage > 1)
             {
                 currentPage--;
-                newLocation += cam.ScreenToWorldPoint(new Vector3(localWidth, 0, 0));
-                print("Old location " + transform.position + " New location " + newLocation);  // REMOVE
+                newLocation += new Vector3(localWidth, 0, 0);
             }
-            StartCoroutine(SmoothMove(transform.position, newLocation, easing));
-            panelLocation = newLocation;
+            // Starting smooth swiping.
+            StartCoroutine(SmoothMove(levelHolderTransform.anchoredPosition, newLocation, easing));
+            lastPanelLocation = newLocation;
         }
         else
         {
-            StartCoroutine(SmoothMove(transform.position, panelLocation, easing));
+            // Returning to the current page.
+            StartCoroutine(SmoothMove(levelHolderTransform.anchoredPosition, lastPanelLocation, easing));
         }
     }
 
@@ -69,7 +74,7 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
         while (t <= 1.0)
         {
             t += Time.deltaTime / seconds;
-            transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0f, 1f, t));
+            levelHolderTransform.anchoredPosition = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0f, 1f, t));
             yield return null;
         }
     }
